c76a8757294447cb27d76fa769ab751015a8780f
escottalexander@gmail.com <escottalexander@gmail.com>
2025-01-10 09:17:16 -0500

    update pragma and path for import

--- extension/packages/foundry/test/MolochRageQuit.t.sol
+++ extension/packages/foundry/test/MolochRageQuit.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.26;
+pragma solidity ^0.8.19;
 
-import "forge-std/Test.sol";
+import "../lib/forge-std/src/Test.sol";
 import "../contracts/MolochRageQuit.sol";
 
 contract MolochRageQuitTest is Test {
