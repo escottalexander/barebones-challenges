8b6af0dd88a4b66cba77f2241e31b2e2386a2cd6
escottalexander@gmail.com <escottalexander@gmail.com>
2025-01-14 15:18:46 -0500

    add reentrancy test

--- extension/packages/foundry/script/DeployContracts.s.sol
+++ extension/packages/foundry/script/DeployContracts.s.sol
@@ -5,12 +5,8 @@ import "../contracts/DeadMansSwitch.sol";
 import "./DeployHelpers.s.sol";
 
 contract DeployContracts is ScaffoldETHDeploy {
-  function run() external ScaffoldEthDeployerRunner {
-    DeadMansSwitch deadMansSwitch = new DeadMansSwitch();
-    console.logString(
-      string.concat(
-        "DeadMansSwitch deployed at: ", vm.toString(address(deadMansSwitch))
-      )
-    );
-  }
+    function run() external ScaffoldEthDeployerRunner {
+        DeadMansSwitch deadMansSwitch = new DeadMansSwitch();
+        console.logString(string.concat("DeadMansSwitch deployed at: ", vm.toString(address(deadMansSwitch))));
+    }
 }
